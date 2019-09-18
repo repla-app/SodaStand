4f12a46d924670a0d2df7a0375fdfffdb0db0069
Roben Kleene <contact@robenkleene.com>
2019-09-18 14:44:52 -0400

    Add a key

---
+++ SodaStand/SodaStand.swift
@@ -0,0 +1,13 @@
+//
+//  SodaStand.swift
+//  SodaStand
+//
+//  Created by Roben Kleene on 9/18/19.
+//  Copyright © 2019 Roben Kleene. All rights reserved.
+//
+
+import Foundation
+
+public class SodaStand {
+    public static let key = "test"
+}
