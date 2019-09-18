de0bf1323c9f312941f21513171bd8525d5e4697
Roben Kleene <contact@robenkleene.com>
2019-09-18 14:24:12 -0400

    First commit

---
+++ SodaStand/SodaStand.h
@@ -0,0 +1,19 @@
+//
+//  SodaStand.h
+//  SodaStand
+//
+//  Created by Roben Kleene on 9/18/19.
+//  Copyright © 2019 Roben Kleene. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SodaStand.
+FOUNDATION_EXPORT double SodaStandVersionNumber;
+
+//! Project version string for SodaStand.
+FOUNDATION_EXPORT const unsigned char SodaStandVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SodaStand/PublicHeader.h>
+
+
